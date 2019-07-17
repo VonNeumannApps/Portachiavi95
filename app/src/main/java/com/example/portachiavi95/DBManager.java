3837dc0e01565b56b0f4d0aae6211db03426dadc
VonNeumannApps <mauro81@gmail.com>
2019-07-17 16:00:17 +0200

    Creato scheletro per classe DBManager per usare Sqlite

--- app/src/main/java/com/example/portachiavi95/DBManager.java
+++ app/src/main/java/com/example/portachiavi95/DBManager.java
@@ -1,6 +1,31 @@
 package com.example.portachiavi95;
 
-public class DBManager {
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class DBManager extends SQLiteOpenHelper {
+
+    /*
+        Usato per gestire i database.
+        Lo useremo per inserire gli account.
+
+     */
+
+    // TODO ci servono nome db e versione
+
+    public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // prima funzione, eseguita solo quando il db non esiste
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // funzionalità che non useremo
+    }
 }
