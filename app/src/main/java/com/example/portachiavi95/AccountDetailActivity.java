82ab6a4fdf114ce4543f40c30d6eade2d0876815
VonNeumannApps <mauro81@gmail.com>
2019-07-18 11:55:54 +0200

    Work in progress, modifica account

--- app/src/main/java/com/example/portachiavi95/AccountDetailActivity.java
+++ app/src/main/java/com/example/portachiavi95/AccountDetailActivity.java
@@ -5,16 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AccountDetailActivity extends AppCompatActivity {
 
     DBManager dbManager;
     Bundle account;
 
+    EditText descET;
+    EditText userET;
+    EditText passET;
+    EditText mailET;
+
+    // carico i valori dell'account nell'EditText
+    void loadAccountData() {
+
+        if (!account.isEmpty()) {
+
+            this.descET.setText(account.getString("descrizione"));
+            this.userET.setText(account.getString("username"));
+            this.passET.setText(account.getString("password"));
+            this.mailET.setText(account.getString("mail"));
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_detail);
+
+        // inizializzo campi EditText dell'account
+        descET = findViewById(R.id.descriptionEditText);
+        userET = findViewById(R.id.usernameEditText);
+        passET = findViewById(R.id.passwordEditText);
+        mailET = findViewById(R.id.emailEditText);
 
         // recupera l'intent con cui sono arrivato a questa activity
         // e ottiene gli extras passati all'intent
@@ -28,7 +53,30 @@ public class AccountDetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO salvare/controllare input utente
+
+                // verificare sela chiamata all'inflater getsystemservice funziona (cambiata per compatibilità API < 23)
+
             }
         });
+
+        loadAccountData();
+    }
+
+    void saveAccountData() {
+
+        // controllare se id esiste già, se no si crea. Usere db manager update o insert new account
+        Boolean isNewAccount = account.isEmpty();
+
+        account.putString("descrizione",  descET.getText().toString());
+        account.putString("username",  userET.getText().toString());
+        account.putString("password",  passET.getText().toString());
+        account.putString("email",  mailET.getText().toString());
+
+        if(isNewAccount) {
+
+            // dbManager.
+        }
+
+
     }
 }
