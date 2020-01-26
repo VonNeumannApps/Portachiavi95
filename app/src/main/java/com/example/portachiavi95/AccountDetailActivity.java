8c81d1e54c257003c31866e61b653e09c9971c47
collideorscopeapps <collideorscopeapps@gmail.com>
2020-01-26 16:55:40 +0100

    unused imports

--- app/src/main/java/com/example/portachiavi95/AccountDetailActivity.java
+++ app/src/main/java/com/example/portachiavi95/AccountDetailActivity.java
@@ -3,13 +3,11 @@ package com.example.portachiavi95;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AccountDetailActivity extends AppCompatActivity {
 
